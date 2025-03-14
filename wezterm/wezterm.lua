4b26ebada42c8620806bb2ae7df64e13f021a1dd
ardaanyazli <ardaanyazli@gmail.com>
2025-03-14 11:19:52 +0300

    Add pwsh specific to windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,17 +2,21 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config = {
-font = wezterm.font('JetBrainsMono Nerd Font Mono'),
-automatically_reload_config = true,
-enable_tab_bar=false,
-window_close_confirmation = 'NeverPrompt',
---window_decorations='RESIZE',
-color_scheme='Vs Code Dark+ (Gogh)',
-  window_padding={
-  left=5,
-  right=5
-  }
+-- Apply your settings to config
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+-- config.window_decorations = "RESIZE"
+config.color_scheme = "Vs Code Dark+ (Gogh)"
+config.window_padding = {
+	left = 5,
+	right = 5,
 }
+
+-- Set PowerShell Core as the default shell on Windows
+if wezterm.target_triple:find("windows") then
+	config.default_prog = { "pwsh.exe" }
+end
 
 return config
