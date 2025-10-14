f5cc35e4389f78ce443b25958d8064444412ee93
ardaanyazli <ardaanyazli@gmail.com>
2025-10-14 15:25:34 +0300

    Change config

--- nvim/lua/plugins/lg-float.lua
+++ nvim/lua/plugins/lg-float.lua
@@ -1,12 +1,12 @@
 return {
 	dir = "~/lg-float.nvim",
-	opts = {
-		width = 0.8,
-		height = 0.8,
-	},
-	keys = {
+	keys = { {
 		"<leader>lg",
 		"<cmd>Lgfloat<CR>",
 		desc = "Open Lazygit",
+	} },
+	opts = {
+		width = 0.8,
+		height = 0.8,
 	},
 }
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -12,7 +12,7 @@ return {
 			nvimtree.setup({
 				view = {
 					width = 35,
-					relativenumber = true,
+					relativenumber = false,
 				},
 				renderer = {
 					indent_markers = {
@@ -30,7 +30,7 @@ return {
 				actions = {
 					open_file = {
 						window_picker = {
-							enable = false,
+							enable = true,
 						},
 					},
 				},
@@ -45,8 +45,12 @@ return {
 			-- Keymaps
 			local keymap = vim.keymap
 			keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-			keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>",
-				{ desc = "Toggle file explorer on current file" })
+			keymap.set(
+				"n",
+				"<leader>ef",
+				"<cmd>NvimTreeFindFileToggle<CR>",
+				{ desc = "Toggle file explorer on current file" }
+			)
 			keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
 			keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 		end,
